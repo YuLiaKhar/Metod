a6f00dd9d9670be7b22984ae1244240f1ce496b8
YuLiaKhar <julia.harchenko02@gmail.com>
2021-10-12 00:56:17 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны первого треугольника");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стороны второго треугольника");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int c2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("S1 = {0:#.##}", Formula(a1, b1, c1));
+            Console.WriteLine("S2 = {0:#.##}", Formula(a2, b2, c2));
+            if (Formula(a1,b1,c1)> Formula(a2,b2,c2))
+            {
+                Console.WriteLine("S первого треугольника больше");
+            }
+            else
+            {
+                Console.WriteLine("S второго треугольника больше");
+            }
+            Console.ReadKey();
+        }
+        static double Formula(int a, int b, int c)
+        {
+            double P = (a + b + c) / 2;
+            double S = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
+            return S;
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int S, V;
+            CubeS(a, out S);
+            Console.WriteLine("S = {0}", S);
+            CubeV(a, out V);
+            Console.WriteLine("V = {0}", V);
+            Console.ReadKey();
+        }
+        static void CubeS(int a, out int S)
+        {
+            S = 6 * a * a;
+        }
+        static void CubeV(int a, out int V)
+        {
+            V = a * a * a;
+        }
+    }
+}
